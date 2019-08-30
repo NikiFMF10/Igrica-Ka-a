da337749f830ffa2fc8d67de9cba3432a8a34367
NikiFMF10 <49947147+NikiFMF10@users.noreply.github.com>
2019-08-30 20:07:03 +0200

    Dodane posodobitve

---
+++ igra.py
@@ -0,0 +1,128 @@
+import turtle
+import random
+
+# smeri kot (x, y) koordinate
+DESNO = (1, 0)
+GOR = (0, 1)
+LEVO = (-1, 0)
+DOL = (0, -1)
+
+# velikost plosce (to je polovica velikosti),
+# celotna plosca ima (VELIKOST_PLOSCE * 2) ** 2 polj
+VELIKOST_PLOSCE = 10
+
+class Igra:
+    def __init__(self):
+        # zacetna smer kace
+        self.smer = DESNO
+        # dovolimo samo eno spremembo smeri na premik
+        self.smer_spremenjena = False
+
+        # koliko hrane je pojedla kača
+        self.rezultat = 0
+
+        # koordinate hrane 
+        self.nova_hrana()
+
+        # pozicija kače, zacetek seznama zacetek kace, 
+        # konec seznama konec kace
+        # naredi novo kačo dolgo 1 v točki (0,0)
+        self.pozicija_kace = [(0, 0)]
+
+        # ali je igre konec
+        self.konec = False
+
+    # naredi novo hrano na nakljucni poziciji
+    # (mogoce bi bilo smiselno preveriti ce ni pod trenutno pozicijo kace)
+    def nova_hrana(self):
+        x = random.randint(-VELIKOST_PLOSCE + 1, VELIKOST_PLOSCE - 1)
+        y = random.randint(-VELIKOST_PLOSCE + 1, VELIKOST_PLOSCE - 1)
+        self.hrana = (x, y)
+
+    # preveri ali se kaca lahko premakne v to polje
+    # vrne True ce je veljavno sicer False
+    def preveri_veljavnost(self, x, y):
+        # preveri ali se je kaca zaletela sama vase
+        for kos_kace in self.pozicija_kace:
+            kos_kace_x, kos_kace_y = kos_kace
+
+
+            if kos_kace_x == x and kos_kace_y == y:
+                # na tem mestu je ze kaca
+                return False
+
+        # preveri ali je kaca cez rob
+        if x <= -VELIKOST_PLOSCE or y <= -VELIKOST_PLOSCE or x >= VELIKOST_PLOSCE or y >= VELIKOST_PLOSCE:
+            return False
+
+        # sicer je vse vredu
+        return True
+
+    # premakne kaco za eno polje naprej v njeni trenutni smeri
+    def premik(self):
+        # trenutna lokacija glave
+        x, y = self.pozicija_kace[0]
+
+        # novo polje dobimo tako da pristejemo smer
+        dx, dy = self.smer
+        novi_x, novi_y = x + dx, y + dy
+
+        # preveri ali se je kaca zaletela v rob ali sama vase
+        if not self.preveri_veljavnost(novi_x, novi_y):
+            # ni veljavno, koncaj igro saj se je kaca zaletela
+            self.konec = True
+
+        # dodaj glavo
+        self.pozicija_kace = [(novi_x, novi_y)] + self.pozicija_kace
+
+        # ali je kaca pojedla hrano
+        if novi_x == self.hrana[0] and novi_y == self.hrana[1]:
+            # dodaj tocke in naredi novo hrano
+            self.rezultat += 1
+            self.nova_hrana()
+        else:
+            # pobrisemo rep
+            self.pozicija_kace.pop()
+
+        # dovoljena nova sprememba smeri
+        self.smer_spremenjena = False
+
+    # funkcije ki obrnejo kaco v neko smer
+    def desno(self):
+        if self.smer == LEVO or self.smer_spremenjena:
+            pass  # kaca se ne obraca za 180 stopinj ali smer je ze bila spremenjena
+        else:
+            self.smer = DESNO
+            self.smer_spremenjena = True
+
+    def gor(self):
+        if self.smer == DOL or self.smer_spremenjena:
+            pass
+        else:
+            self.smer = GOR
+            self.smer_spremenjena = True
+
+    def levo(self):
+        if self.smer == DESNO or self.smer_spremenjena:
+            pass
+        else:
+            self.smer = LEVO
+            self.smer_spremenjena = True
+
+    def dol(self):
+        if self.smer == GOR or self.smer_spremenjena:
+            pass
+        else:
+            self.smer = DOL
+            self.smer_spremenjena = True
+
+    # trenutna smer zelve v stopinjah
+    def smer_v_stopinjah(self):
+        if self.smer == DESNO:
+            return 0
+        elif self.smer == GOR:
+            return 90
+        elif self.smer == LEVO:
+            return 180
+        else:
+            return 270
---
+++ vmesnik.py
@@ -0,0 +1,128 @@
+from igra import Igra, VELIKOST_PLOSCE
+import turtle
+
+# sirina polja v pikslih
+SIRINA_POLJA = 20
+
+# kolikokrat na sekundo se kaca premakne
+FRAME_RATE = 10
+
+class Vmesnik:
+    def __init__(self):
+        turtle.hideturtle()
+        turtle.clear()
+        turtle.pu()
+        turtle.color('red')
+        turtle.goto(0,0)
+        turtle.write('Igraj')
+        turtle.title('Igra Kača')
+        turtle.onscreenclick(self.nova_igra)
+
+        # slika se posodobi samo vsakih 1000 premikov, ali ko jo posodobimo rocno
+        turtle.tracer(n=1000)
+        
+        turtle.mainloop()
+
+    def nova_igra(self, *_):
+        # naredi novo igro
+        self.igra = Igra()
+
+        turtle.onscreenclick(None)
+        self.okno()
+
+        # zelva ki rise hrano
+        self.hrana = turtle.Turtle()
+        self.hrana.hideturtle()
+        self.hrana.pu()
+        self.hrana.speed(0)
+        self.hrana.shape('square')
+        self.hrana.color('red')
+
+        # zelva ki rise kaco
+        self.kaca = turtle.Turtle()
+        self.kaca.hideturtle()
+        self.kaca.pu()
+        self.kaca.speed(1)
+        self.kaca.shape('square')
+        self.kaca.color('green')
+
+        # zelva ki rise rezultat
+        self.rezultat = turtle.Turtle()
+        self.rezultat.hideturtle()
+        self.rezultat.pu()
+        self.rezultat.speed(0)
+        self.rezultat.goto(100,-250)
+
+        # poslusaj na pritisk tipke        
+        turtle.onkey(self.igra.gor, 'Up')
+        turtle.onkey(self.igra.levo, 'Left')
+        turtle.onkey(self.igra.desno, 'Right')
+        turtle.onkey(self.igra.dol, 'Down')
+        turtle.listen()
+
+        self.narisi()
+
+    def okno(self):
+        turtle.clear()
+        turtle.pu()
+        turtle.speed(0)
+        turtle.pensize(SIRINA_POLJA)
+        turtle.color('black')
+        turtle.goto(-VELIKOST_PLOSCE * SIRINA_POLJA, VELIKOST_PLOSCE * SIRINA_POLJA)
+        turtle.pd()
+        turtle.goto(VELIKOST_PLOSCE * SIRINA_POLJA, VELIKOST_PLOSCE * SIRINA_POLJA)
+        turtle.goto(VELIKOST_PLOSCE * SIRINA_POLJA, -VELIKOST_PLOSCE * SIRINA_POLJA)
+        turtle.goto(-VELIKOST_PLOSCE * SIRINA_POLJA, -VELIKOST_PLOSCE * SIRINA_POLJA)
+        turtle.goto(-VELIKOST_PLOSCE * SIRINA_POLJA, VELIKOST_PLOSCE * SIRINA_POLJA)
+        turtle.pu()
+        turtle.goto(0,0)
+
+    def narisi(self):
+        # premakni kaco
+        self.igra.premik()
+
+        # narisi hrano
+        self.hrana.clear()
+        self.hrana.goto(self.igra.hrana[0] * SIRINA_POLJA, self.igra.hrana[1] * SIRINA_POLJA)
+        self.hrana.stamp()
+
+        # narisi vse dele kace
+        self.kaca.clear()
+        for kos_kace in self.igra.pozicija_kace:
+            self.kaca.goto(kos_kace[0] * SIRINA_POLJA, kos_kace[1] * SIRINA_POLJA)
+            self.kaca.stamp()
+        
+        # narisi rezultat
+        self.rezultat.clear()
+        self.rezultat.write('Rezultat: ' + str(self.igra.rezultat), align='center', font=(10))
+
+        # konco zares narisi vse na zaslon
+        turtle.update()
+
+        # konec igre
+        if self.igra.konec:      
+            self.rezultat.clear()
+            self.hrana.clear()
+            self.kaca.clear()
+            self.konecigre()
+
+        # narisi ponovno cez t milisekund
+        turtle.ontimer(self.narisi, t=int(1000 / FRAME_RATE))
+
+    def konecigre(self):
+        turtle.onscreenclick(None)
+        turtle.speed(0)
+        turtle.pu()
+        turtle.goto(0,150)
+        turtle.color('red')
+        turtle.write('Konec igre', align='center', font=(15))
+        turtle.goto(0,50)
+        turtle.write('Tvoj rezultat: ' + str(self.igra.rezultat), align='center', font = (10))
+        turtle.goto(180,-180)
+        turtle.write('Poskusi znova', align='right', font=(0.0000001))
+        turtle.onscreenclick(self.nova_igra)
+        turtle.mainloop()
+
+
+if __name__ == '__main__':
+    vmesnik = Vmesnik()
